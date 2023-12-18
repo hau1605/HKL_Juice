80f210429e7b95f083d0ad15698e0b58fd427012
minghunt <114232628+minghunt@users.noreply.github.com>
2023-12-19 02:00:29 +0700

    add admin home page

---
+++ HKL_Juice/Routes/AdminRoute.cs
@@ -0,0 +1,21 @@
+﻿using HKL_Juice.Models;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HKL_Juice.Routes
+{
+    public class AdminRoute : NancyModule
+    {
+        public AdminRoute(ApplicationDbContext dbContext)
+        {
+            Get("/admin", parameters => {
+                return View["admin.cshtml"];
+            }
+            );
+
+        }
+    }
+}
