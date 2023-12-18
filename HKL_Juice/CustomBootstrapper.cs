996293d682e553daf272e463d6bc9b15d1f26f8a
minghunt <114232628+minghunt@users.noreply.github.com>
2023-12-18 20:02:39 +0700

    nm

---
+++ HKL_Juice/CustomBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using Nancy.ViewEngines;
+using Nancy.ViewEngines.Razor;
+namespace HKL_Juice
+{
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            // Cấu hình thêm cho container nếu cần
+        }
+
+        protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+        {
+            base.ConfigureRequestContainer(container, context);
+            container.Register<IRazorConfiguration>(new DefaultRazorConfiguration());
+        }
+
+    }
+
+}
