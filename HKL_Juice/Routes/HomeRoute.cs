cc3e61f1bf38420d38666767af6feccac7f47fb2
hau1605 <phmh165@gmail.com>
2023-12-28 01:48:14 +0700

    updateee layout

--- HKL_Juice/Routes/HomeRoute.cs
+++ HKL_Juice/Routes/HomeRoute.cs
@@ -1,9 +1,10 @@
-﻿using HKL_Juice.Models;
-using Nancy;
+﻿using Nancy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
+using System.Data.Entity;
 
 namespace HKL_Juice.Routes
 {
@@ -19,9 +20,23 @@ namespace HKL_Juice.Routes
             );*/
             Get("/", parameters => {
 
-                return View["home.cshtml"];
-            }
-            );
+                var products = dbContext.Product
+                   .Select(p => new
+                   {
+                       productId = p.productId,
+                       categoryId = p.categoryId,
+                       productName = p.productName,
+                       price = p.price,
+                       descript = p.descript,
+                       imgUrl = p.imgUrl,
+                       OrderDetails = p.Category.categoryName
+                   }).ToList();
+
+                var serializer = new JavaScriptSerializer();
+                string json = serializer.Serialize(products);
+
+                return View["home.cshtml", json];
+            });
         }
     }
 }
