ae1867b91ecadd69e5dbc74ce10df6c8aa3cf3b6
minghunt <114232628+minghunt@users.noreply.github.com>
2024-01-07 11:38:26 +0700

    add redirect

--- HKL_Juice/Routes/HomeRoute.cs
+++ HKL_Juice/Routes/HomeRoute.cs
@@ -18,6 +18,13 @@ namespace HKL_Juice.Routes
                 return View["historyOrdered.cshtml"];
             }
             );
+            Get("/redirect/{id}", parameters => {
+                int numberTable = parameters.id;
+                var serializer = new JavaScriptSerializer();
+                string json = serializer.Serialize(numberTable);
+                return View["redirect.cshtml", json];
+            }
+            );
             /*Get("/", parameters => {
 
                 var products = dbContext.Product
