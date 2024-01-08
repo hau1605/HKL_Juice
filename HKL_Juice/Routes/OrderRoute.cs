5fcaa69928c2b8ea055906fd073a9994c0aff10b
hau1605 <phmh165@gmail.com>
2024-01-08 11:04:44 +0700

    try catch, null data

--- HKL_Juice/Routes/OrderRoute.cs
+++ HKL_Juice/Routes/OrderRoute.cs
@@ -31,34 +31,39 @@ namespace HKL_Juice.Routes
         {
             Get("/api/v1/orders/{numberTable}", parameters =>
             {
-                int numberTable = parameters.numberTable;
-                var orders = dbContext.Order
-                    .Where(o => o.numberTable == numberTable && 
-                                (o.paymentStatus == "Đang chờ" || o.paymentStatus == "Đã thanh toán"))
-                    .OrderByDescending(o => o.orderDate)
-                    .Select(o => new
-                    {
-                        paymentStatus = o.paymentStatus,
-                        orderTotal = o.orderTotal,
-                        orderStatus = o.orderStatus,
-                        OrderDetails = o.OrderDetails.Select(od => new
+                try
+                {
+                    int numberTable = parameters.numberTable;
+                    var orders = dbContext.Order
+                        .Where(o => o.numberTable == numberTable &&
+                                    (o.paymentStatus == "Đang chờ" || o.paymentStatus == "Đã thanh toán"))
+                        .OrderByDescending(o => o.orderDate)
+                        .Select(o => new
                         {
-                            productName = od.Product.productName,
-                            price = od.Product.price,
-                            quantity = od.quantity,
-                            subTotal = od.subTotal,
-                            imgUrl = od.Product.imgUrl
-                        }).ToList()
-                    }).ToList();
+                            paymentStatus = o.paymentStatus,
+                            orderTotal = o.orderTotal,
+                            orderStatus = o.orderStatus,
+                            OrderDetails = o.OrderDetails.Select(od => new
+                            {
+                                productName = od.Product.productName,
+                                price = od.Product.price,
+                                quantity = od.quantity,
+                                subTotal = od.subTotal,
+                                imgUrl = od.Product.imgUrl
+                            }).ToList()
+                        }).ToList();
 
-                var serializer = new JavaScriptSerializer();
-                string json = serializer.Serialize(orders);
-                return Response.AsJson(orders);
-                /*return View["orderAdmin.cshtml", json];*/
+                    return Response.AsJson(orders);
+                } catch
+                {
+                    return HttpStatusCode.BadRequest;
+                }
             });
             Get("/api/v1/order-details", parameters =>
             {
-                var orderDetails = dbContext.OrderDetail
+                try
+                {
+                    var orderDetails = dbContext.OrderDetail
                                   .Select(od => new
                                   {
                                       orderDetailId = od.orderDetailId,
@@ -67,94 +72,28 @@ namespace HKL_Juice.Routes
                                       quantity = od.quantity,
                                       subTotal = od.subTotal
                                   }).ToList();
-                var serializer = new JavaScriptSerializer();
-                string json = serializer.Serialize(orderDetails);
-                return Response.AsJson(orderDetails);
+                    var serializer = new JavaScriptSerializer();
+                    string json = serializer.Serialize(orderDetails);
+                    return Response.AsJson(orderDetails);
+                }
+                catch
+                {
+                    return HttpStatusCode.BadRequest;
+                }
             });
             Post("/api/v1/order", (parameters) =>
             {
-                var createOrder = this.Bind<CreateOrder>();
-                var order = new Order()
-                {
-                    orderDate = DateTime.Now,
-                    paymentStatus = createOrder.paymentStatus,
-                    userId = 1,
-                    paymentMethod = "Tiền mặt",
-                    orderTotal = createOrder.orderTotal,
-                    numberTable = createOrder.numberTable
-                };
-                var createdOrder = dbContext.Order.Add(order);
-                dbContext.SaveChanges();
-                var orderId = order.orderId;
-
-                var orderDetail = createOrder.orderDetail;
-                foreach (var detail in orderDetail)
-                {
-                    var newDetail = new OrderDetail
-                    {
-                        orderId = orderId,
-                        productId = detail.productId,
-                        quantity = detail.quantity,
-                        subTotal = detail.subTotal
-                    };
-                    var createdOrderDetail = dbContext.OrderDetail.Add(newDetail);
-                    dbContext.SaveChanges();
-                }
-
-                return HttpStatusCode.OK;
-            });
-
-            Post("/api/v1/order/{numberTable}", (parameters) =>
-            {
-                int numberTable = parameters.numberTable;
-                var existingOrder = dbContext.Order
-                    .Where(o => o.numberTable == numberTable && o.paymentStatus == "Đang chờ")
-                    .OrderByDescending(o => o.orderDate)
-                    .FirstOrDefault();
-                if (existingOrder != null) // Cập nhật
-                {
-                    var updateOrder = this.Bind<CreateOrder>();
-                    existingOrder.orderDate = DateTime.Now;
-                    existingOrder.orderTotal += updateOrder.orderTotal;
-                    existingOrder.orderStatus = "Đơn mới";
-
-                    /*var updatedOrder = dbContext.Order.Add(existingOrder);*/
-                    var updatedOrder = existingOrder;
-                    dbContext.SaveChanges();
-                    var orderId = existingOrder.orderId;
-
-                    foreach (var detail in updateOrder.orderDetail)
-                    {
-                        var updateDetail = new OrderDetail
-                        {
-                            orderId = orderId,
-                            productId = detail.productId,
-                            quantity = detail.quantity,
-                            subTotal = detail.subTotal,
-                            isNew = true
-                        };
-                        var updatedDetail = dbContext.OrderDetail.Add(updateDetail);
-                        dbContext.SaveChanges();
-                    }
-                    var sdas11 = updateOrder;
-                    return Response.AsJson(new
-                    {
-                        Success = true,
-                        Message = "Cập nhật"
-                    });
-                }
-                else // Update
+                try
                 {
                     var createOrder = this.Bind<CreateOrder>();
                     var order = new Order()
                     {
-                        userId = 1,
                         orderDate = DateTime.Now,
-                        paymentMethod = "Tiền mặt",
-                        orderStatus = "Đơn mới",
                         paymentStatus = createOrder.paymentStatus,
+                        userId = 1,
+                        paymentMethod = "Tiền mặt",
                         orderTotal = createOrder.orderTotal,
-                        numberTable = numberTable,
+                        numberTable = createOrder.numberTable
                     };
                     var createdOrder = dbContext.Order.Add(order);
                     dbContext.SaveChanges();
@@ -168,18 +107,101 @@ namespace HKL_Juice.Routes
                             orderId = orderId,
                             productId = detail.productId,
                             quantity = detail.quantity,
-                            subTotal = detail.subTotal,
-                            isNew = true
+                            subTotal = detail.subTotal
                         };
                         var createdOrderDetail = dbContext.OrderDetail.Add(newDetail);
                         dbContext.SaveChanges();
                     }
-                    return Response.AsJson(new 
-                    { 
-                        Success = true, 
-                        Message = "Tạo mới",
-                        invoice = createOrder 
-                    });
+
+                    return HttpStatusCode.OK;
+                } catch
+                {
+                    return HttpStatusCode.BadRequest;
+                }
+            });
+
+            Post("/api/v1/order/{numberTable}", (parameters) =>
+            {
+                try
+                {
+                    int numberTable = parameters.numberTable;
+                    var existingOrder = dbContext.Order
+                        .Where(o => o.numberTable == numberTable && o.paymentStatus == "Đang chờ")
+                        .OrderByDescending(o => o.orderDate)
+                        .FirstOrDefault();
+                    if (existingOrder != null) // Cập nhật
+                    {
+                        var updateOrder = this.Bind<CreateOrder>();
+                        existingOrder.orderDate = DateTime.Now;
+                        existingOrder.orderTotal += updateOrder.orderTotal;
+                        existingOrder.orderStatus = "Đơn mới";
+
+                        /*var updatedOrder = dbContext.Order.Add(existingOrder);*/
+                        var updatedOrder = existingOrder;
+                        dbContext.SaveChanges();
+                        var orderId = existingOrder.orderId;
+
+                        foreach (var detail in updateOrder.orderDetail)
+                        {
+                            var updateDetail = new OrderDetail
+                            {
+                                orderId = orderId,
+                                productId = detail.productId,
+                                quantity = detail.quantity,
+                                subTotal = detail.subTotal,
+                                isNew = true
+                            };
+                            var updatedDetail = dbContext.OrderDetail.Add(updateDetail);
+                            dbContext.SaveChanges();
+                        }
+                        var sdas11 = updateOrder;
+                        return Response.AsJson(new
+                        {
+                            Success = true,
+                            Message = "Cập nhật"
+                        });
+                    }
+                    else // Update
+                    {
+                        var createOrder = this.Bind<CreateOrder>();
+                        var order = new Order()
+                        {
+                            userId = 1,
+                            orderDate = DateTime.Now,
+                            paymentMethod = "Tiền mặt",
+                            orderStatus = "Đơn mới",
+                            paymentStatus = createOrder.paymentStatus,
+                            orderTotal = createOrder.orderTotal,
+                            numberTable = numberTable,
+                        };
+                        var createdOrder = dbContext.Order.Add(order);
+                        dbContext.SaveChanges();
+                        var orderId = order.orderId;
+
+                        var orderDetail = createOrder.orderDetail;
+                        foreach (var detail in orderDetail)
+                        {
+                            var newDetail = new OrderDetail
+                            {
+                                orderId = orderId,
+                                productId = detail.productId,
+                                quantity = detail.quantity,
+                                subTotal = detail.subTotal,
+                                isNew = true
+                            };
+                            var createdOrderDetail = dbContext.OrderDetail.Add(newDetail);
+                            dbContext.SaveChanges();
+                        }
+                        return Response.AsJson(new
+                        {
+                            Success = true,
+                            Message = "Tạo mới",
+                            invoice = createOrder
+                        });
+                    }
+                } catch
+                {
+                    return HttpStatusCode.BadRequest;
                 }
             });
         }
